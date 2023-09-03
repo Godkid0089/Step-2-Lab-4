2be73b409a1d5a5a28bc71bee47b0af23a926856
YooZ <lolokino0089@gmail.com>
2023-09-03 12:37:28 +0800

    lab

---
+++ app.js
@@ -0,0 +1,26 @@
+/*console.log(document);
+// document.GetElementsByTagName()->tag nereer handah
+var h1 = document.getElementsByTagName('h1')[0];
+console.log(h1);
+h1.innerText="hello";
+h1.style.color="blue";
+
+var j1 = document.getElementsByTagName('H1')[1];
+console.log(h1);
+j1.innerText="mint";
+j1.style.color="pink";
+var p = document.getElementsByTagName('p')[0];
+console.log(p);
+function (){
+    p.innertext = "click ";
+    p.style.color = "green";
+} */
+
+var spooderman = document.getElementsByTagName('input')[0];
+var spooderman2 = document.getElementsByTagName('h4')[0];
+var now = 2023,myAge;
+console.log(spooderman2);
+console.log(spooderman);
+function nasaa_oruul(){
+
+}
